a6203e4eb49cde2d72b99b0dced3ba4d5f3b1b89
Nikolay Gorshkov <37116427+Nicelx@users.noreply.github.com>
2021-01-29 23:50:28 +0300

    daily redux

--- src/app/Navbar.js
+++ src/app/Navbar.js
@@ -1,15 +1,19 @@
 import React from 'react'
 
-export const Navbar = () => {
-  return (
-    <nav>
-      <section>
-        <h1>Redux Essentials Example</h1>
+import { Link } from 'react-router-dom'
 
-        <div className="navContent">
-          <div className="navLinks"></div>
-        </div>
-      </section>
-    </nav>
-  )
+export const Navbar = () => {
+    return (
+        <nav>
+            <section>
+                <h1>Redux Essentials Example</h1>
+
+                <div className="navContent">
+                    <div className="navLinks">
+                        <Link to="/">Posts</Link>
+                    </div>
+                </div>
+            </section>
+        </nav>
+    )
 }
